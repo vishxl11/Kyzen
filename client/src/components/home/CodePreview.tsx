a18a6434757cd10933882f01724570c6a4e0c49e
vishxl11 <vishaldudhal11@gmail.com>
2026-06-01 08:49:48 +0530

    built landing page with animated preview

---
+++ client/src/components/home/CodePreview.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+
+const username = `"vishxl_11";`;
+
+export default function CodePreview() {
+
+  const [text, setText] = useState("");
+  const [deleting, setDeleting] = useState(false);
+
+  useEffect(() => {
+
+    let timeout: number;
+
+    if (!deleting) {
+
+      if (text.length < username.length) {
+
+        timeout = window.setTimeout(() => {
+
+          setText(
+            username.slice(0, text.length + 1)
+          );
+
+        }, 100);
+
+      } else {
+
+        timeout = window.setTimeout(() => {
+
+          setDeleting(true);
+
+        }, 1200);
+
+      }
+
+    } else {
+
+      if (text.length > 0) {
+
+        timeout = window.setTimeout(() => {
+
+          setText(
+            text.slice(0, -1)
+          );
+
+        }, 60);
+
+      } else {
+
+        timeout = window.setTimeout(() => {
+
+          setDeleting(false);
+
+        }, 500);
+
+      }
+
+    }
+
+    return () => clearTimeout(timeout);
+
+  }, [text, deleting]);
+
+  return (
+    <div
+      className="
+        relative
+        p-8
+        font-mono
+        text-[18px]
+        leading-8
+        text-zinc-300
+      "
+    >
+
+      <div>
+        <span className="text-pink-400">
+          #include
+        </span>
+
+        {" <bits/stdc++.h>"}
+      </div>
+
+      <div>
+        <span className="text-cyan-300">
+          using
+        </span>
+
+        {" namespace std;"}
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        <span className="text-emerald-300">
+          int
+        </span>
+
+        {" main() {"}
+      </div>
+
+     <div>
+  {"    string user = "}
+
+  <span className="text-amber-300">
+    {text}
+  </span>
+</div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        {'    cout << "Welcome to KyZen";'}
+      </div>
+
+      <div>&nbsp;</div>
+
+      <div>
+        {"    return 0;"}
+      </div>
+
+      <div>{"}"}</div>
+
+      {/* Collaborator */}
+
+      <div
+        className="
+          absolute
+          transition-all
+          duration-100
+        "
+        style={{
+          top: "130px",
+          left: `${180 + text.length * 8.6}px`,
+        }}
+      >
+
+        <div
+          className="
+            bg-pink-500
+            text-white
+            text-[10px]
+            px-2
+            py-1
+            rounded-md
+            font-semibold
+          "
+        >
+          vishxl_11
+        </div>
+
+        <div
+          className="
+            ml-2
+            w-[2px]
+            h-5
+            bg-pink-500
+          "
+        />
+
+      </div>
+
+    </div>
+  );
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,228 @@
-export default function Home()
+import CodePreview from "../components/home/CodePreview";
+export default function Room()
 {
-    return(
-        <div>
-            <h1 className="bg-amber-500">HOME PAGE</h1>
-        </div>
-    )
-}
+return (
+  <div className="min-h-screen bg-[#050816] text-white">
 
+    <nav className="h-16 border-b border-zinc-900 flex items-center px-8">
+
+      <h1 className="text-2xl font-bold">
+        <span>Ky</span>
+        <span className="text-emerald-400">
+          Zen
+        </span>
+      </h1>
+
+    </nav>
+
+    <section className="max-w-350 mx-auto px-8 h-[calc(100vh-4rem)] flex items-center">
+
+      <div className="w-[45%]">
+
+<h1 className="text-6xl font-bold leading-tight">
+
+  <span className="text-white">
+    Code
+  </span>
+
+  <span
+    className="
+      bg-gradient-to-r
+      from-emerald-400
+      to-teal-300
+      bg-clip-text
+      text-transparent
+    "
+  >
+    {" "}Together.
+  </span>
+
+  <br />
+
+  <span className="text-white">
+    Execute
+  </span>
+
+  <span
+    className="
+      bg-gradient-to-r
+      from-emerald-400
+      to-teal-300
+      bg-clip-text
+      text-transparent
+    "
+  >
+    {" "}Instantly.
+  </span>
+
+</h1>
+
+        <p className="mt-6 text-zinc-400 text-lg max-w-130">
+          Real-time collaborative coding with shared execution,
+          synchronized collaboration and room-based sessions.
+        </p>
+
+
+        <div
+  className="
+    mt-10
+    w-full
+    max-w-[460px]
+    rounded-3xl
+    p-0
+  "
+>
+
+  <div>
+    <label className="text-sm text-zinc-400">
+      Display Name
+    </label>
+
+    <input
+      type="text"
+      placeholder="vishxl_11"
+      className="
+        mt-2
+        w-full
+        h-12
+        px-4
+        rounded-xl
+        bg-[#111827]
+        border
+        border-zinc-700
+        outline-none
+        focus:border-emerald-400
+      "
+    />
+  </div>
+
+  <div className="my-6 h-px bg-zinc-800" />
+
+  <div>
+    <label className="text-sm text-zinc-400">
+      Join Existing Room
+    </label>
+
+    <div className="flex gap-3 mt-2">
+
+      <input
+        type="text"
+        placeholder="Room Code"
+        className="
+          flex-1
+          h-12
+          px-4
+          rounded-xl
+          bg-[#111827]
+          border
+          border-zinc-700
+          outline-none
+          focus:border-emerald-400
+        "
+      />
+
+      <button
+        className="
+          px-6
+          rounded-xl
+          bg-zinc-800
+          hover:bg-zinc-700
+          transition-colors
+        "
+      >
+        Join
+      </button>
+
+    </div>
+  </div>
+
+  <div className="my-6 flex items-center">
+
+    <div className="flex-1 h-px bg-zinc-800" />
+
+    <span className="px-3 text-xs text-zinc-500">
+      OR
+    </span>
+
+    <div className="flex-1 h-px bg-zinc-800" />
+
+  </div>
+
+  <button
+    className="
+      w-full
+      h-12
+      rounded-xl
+      bg-gradient-to-r
+      from-emerald-500
+      to-teal-400
+      text-white
+      font-semibold
+      hover:opacity-90
+      transition-all
+    "
+  >
+    Create New Room
+  </button>
+
+</div>
+
+      </div>
+
+    
+   <div className="w-[55%] flex justify-center items-center">
+
+  <div
+    className="
+      w-[850px]
+      h-[520px]
+      rounded-3xl
+      bg-[#0B1220]
+      border border-zinc-800/60
+      overflow-hidden
+      shadow-[0_20px_60px_rgba(0,0,0,0.35)]
+    "
+  >
+
+      {/* Top Bar */}
+
+      <div className="h-12 border-b border-zinc-800 flex items-center px-5 gap-2">
+
+        <div className="w-3 h-3 rounded-full bg-red-500" />
+        <div className="w-3 h-3 rounded-full bg-yellow-500" />
+        <div className="w-3 h-3 rounded-full bg-emerald-500" />
+
+        <div
+          className="
+            ml-4
+            px-3
+            py-1
+            rounded-lg
+            bg-[#111827]
+            text-xs
+            text-zinc-400
+          "
+        >
+          main.cpp
+        </div>
+
+      </div>
+
+      {/* Preview */}
+
+      <div className="p-8">
+
+        <CodePreview />
+
+      </div>
+
+  </div>
+
+</div>
+    
+
+    </section>
+
+  </div>
+);
+}
